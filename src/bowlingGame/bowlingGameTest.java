4630ac68314b959a90be3b7eaa487071098ef7b4
epson220 <freshsangmin@ajou.ac.kr>
2020-05-05 02:35:37 +0900

    first commit

---
+++ src/bowlingGame/bowlingGameTest.java
@@ -0,0 +1,47 @@
+package bowlingGame;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class bowlingGameTest {
+	private Game g;
+	
+	@Before
+	public void setUp() {
+		g = new Game();
+	}
+	
+	@Test
+	public void testGutterGame() {
+	
+		rollMany(20, 0);
+		
+		assertEquals(0, g.score());
+	}
+	
+	private void rollMany(int n, int pins) {
+		for(int i=0; i<n; i++) {
+			g.roll(pins);
+		}
+	}
+	
+	@Test
+	public void testAllOnes() {
+		
+		rollMany(20, 1);
+		assertEquals(20, g.score());
+	}
+	
+	@Ignore("until we get design right")
+	@Test
+	public void testOneSpare() {
+		g.roll(5);
+		g.roll(5);
+		g.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, g.score());
+	}
+}
